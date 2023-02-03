abc3f52ecefd6f11ef12017267821395c1265508
eaxis <4228431+eaxis@users.noreply.github.com>
2023-02-03 20:47:53 +0300

    Update HtmlSanitizerExtension.php

--- src/HtmlSanitizerExtension.php
+++ src/HtmlSanitizerExtension.php
@@ -8,14 +8,16 @@ class HtmlSanitizerExtension extends SimpleExtension
 {
     protected function registerTwigFilters()
     {
+        $options = ['is_safe' => [ 'html' ], 'safe' => true];
+
         return [
-            'sanitize' => ['sanitize']
+            'sanitize' => ['sanitize', $options]
         ];
     }
 
     public function sanitize($string)
     {
-        $allowedMarkup = '<br><p><h1><h2><h3><h4><a><strong><ul><i><b><u><li><div><button>';
+        $allowedMarkup = '<br><p><h1><h2><h3><h4><a><strong><ul><i><b><u><li><div><img>';
         $string = \strip_tags($string, $allowedMarkup);
 
         $string = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $string);
